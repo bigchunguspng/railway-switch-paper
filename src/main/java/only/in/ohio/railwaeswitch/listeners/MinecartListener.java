8d22a9ef039990fd576dba24b428f331524b0ffc
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-11-05 01:03:00 +0200

    rails are switched in passenger's facing direction

--- src/main/java/only/in/ohio/railwaeswitch/listeners/MinecartListener.java
+++ src/main/java/only/in/ohio/railwaeswitch/listeners/MinecartListener.java
@@ -1,6 +1,5 @@
 package only.in.ohio.railwaeswitch.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -10,6 +9,9 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class MinecartListener implements Listener
 {
@@ -30,9 +32,7 @@ public class MinecartListener implements Listener
 
         if (blockIsTwoWaySwitch(rails))
         {
-            var direction = getMinecartDirection(getVelocity(event));
-
-            Bukkit.getLogger().info(direction.toString());
+            var direction = getCartDirection(getVelocity(event));
 
             var next = rails.getRelative(direction);
             if (next.getType() != Material.RAIL) return;
@@ -41,9 +41,7 @@ public class MinecartListener implements Listener
             if (next.getBlockData().getAsString().contains(SHAPE_ASC)) return;
 
             var yaw = passenger.getLocation().getYaw();
-            var shape = getRailShape(direction, yaw);
-
-            setRailShape(next, shape);
+            setRailShape(next, getRailShape(direction, yaw));
         }
     }
 
@@ -58,27 +56,76 @@ public class MinecartListener implements Listener
         return event.getTo().subtract(event.getFrom());
     }
 
-    private static BlockFace getMinecartDirection(Location speed)
+    private static BlockFace getCartDirection(Location speed)
     {
         if /**/ (speed.getX() != 0) return speed.getX() > 0 ? BlockFace.EAST : BlockFace.WEST;
         else if (speed.getZ() != 0) return speed.getZ() > 0 ? BlockFace.SOUTH : BlockFace.NORTH;
 
-        else return BlockFace.DOWN;
+        else return BlockFace.SELF;
     }
 
-    private static Rail.Shape getRailShape(BlockFace minecartDirection, float playerYaw)
+    private static Rail.Shape getRailShape(BlockFace minecartDirection, float passengerYaw)
     {
-        switch (minecartDirection)
+        // todo select only valid directions (1-3)
+
+        var routes = new ArrayList<BlockFace>();
+        routes.add(BlockFace.NORTH);
+        routes.add(BlockFace.EAST);
+        routes.add(BlockFace.SOUTH);
+        routes.add(BlockFace.WEST);
+
+        // can't turn backwards
+        routes.remove(minecartDirection.getOppositeFace());
+
+        var angles = routes.stream().map(x -> angleDifference(passengerYaw, getDirectionYaw(x))).collect(Collectors.toList());
+
+        var closest = angles.stream().min(Float::compareTo);
+        var index = closest.map(angles::indexOf).orElse(0);
+        var route = routes.get(index);
+
+        return getRailShape(minecartDirection, route);
+    }
+
+    public static float getDirectionYaw(BlockFace direction)
+    {
+        return switch (direction)
         {
-            case NORTH:
-                return Rail.Shape.SOUTH_EAST;
-            case EAST:
-                return Rail.Shape.SOUTH_WEST;
-            case SOUTH:
-                return Rail.Shape.NORTH_WEST;
-            default:
-                return Rail.Shape.NORTH_EAST;
+            case EAST -> -90;
+            case SOUTH -> 0;
+            case WEST -> 90;
+            default -> 180;
+        };
+    }
+
+    private static float angleDifference(float yaw1, float yaw2)
+    {
+        var abs = Math.abs(yaw1 - yaw2);
+        return abs > 180 ? 360 - abs : abs;
+    }
+
+    private static Rail.Shape getRailShape(BlockFace straight, BlockFace turn)
+    {
+        if (straight == turn)
+        {
+            return straight.ordinal() % 2 == 0 ? Rail.Shape.NORTH_SOUTH : Rail.Shape.EAST_WEST;
         }
+        if (straight == BlockFace.NORTH)
+        {
+            return turn == BlockFace.EAST ? Rail.Shape.SOUTH_EAST : Rail.Shape.SOUTH_WEST;
+        }
+        if (straight == BlockFace.SOUTH)
+        {
+            return turn == BlockFace.EAST ? Rail.Shape.NORTH_EAST : Rail.Shape.NORTH_WEST;
+        }
+        if (straight == BlockFace.EAST)
+        {
+            return turn == BlockFace.NORTH ? Rail.Shape.NORTH_WEST : Rail.Shape.SOUTH_WEST;
+        }
+        if (straight == BlockFace.WEST)
+        {
+            return turn == BlockFace.NORTH ? Rail.Shape.NORTH_EAST : Rail.Shape.SOUTH_EAST;
+        }
+        return Rail.Shape.EAST_WEST; // <-- literally anything
     }
 
     private static void setRailShape(Block rail, Rail.Shape shape)
