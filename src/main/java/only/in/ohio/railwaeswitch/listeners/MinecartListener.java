cc77bb07d8e653ab2b8f6b488b4c5df11f1fd97a
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-11-04 19:32:10 +0200

    ###

--- src/main/java/only/in/ohio/railwaeswitch/listeners/MinecartListener.java
+++ src/main/java/only/in/ohio/railwaeswitch/listeners/MinecartListener.java
@@ -3,6 +3,7 @@ package only.in.ohio.railwaeswitch.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Rail;
 import org.bukkit.entity.EntityType;
@@ -18,44 +19,46 @@ public class MinecartListener implements Listener
     public void onVehicleMove(VehicleMoveEvent event)
     {
         var vehicle = event.getVehicle();
-        if (vehicle.getType() == EntityType.MINECART)
+        if (vehicle.getType() != EntityType.MINECART) return;
+
+        var passengers = vehicle.getPassengers();
+        if (passengers.isEmpty()) return;
+
+        var passenger = passengers.get(0);
+
+        var rails = vehicle.getLocation().getBlock();
+
+        if (blockIsTwoWaySwitch(rails))
         {
-            var passengers = vehicle.getPassengers();
-            if (!passengers.isEmpty())
-            {
-                var passenger = passengers.get(0);
+            var direction = getMinecartDirection(getVelocity(event));
 
-                var rails = vehicle.getLocation().getBlock();
-                var floor = rails.getRelative(BlockFace.DOWN);
+            Bukkit.getLogger().info(direction.toString());
 
-                if (rails.getType() == Material.ACTIVATOR_RAIL && floor.getType() == Material.IRON_BLOCK)
-                {
-                    var velocity = event.getTo().subtract(event.getFrom());
-                    var direction = GetMinecartDirection(velocity);
+            var next = rails.getRelative(direction);
+            if (next.getType() != Material.RAIL) return;
 
-                    Bukkit.getLogger().info(velocity.toString());
-                    Bukkit.getLogger().info(direction.toString());
+            // ascending tracks can't be switched
+            if (next.getBlockData().getAsString().contains(SHAPE_ASC)) return;
 
-                    var next = rails.getRelative(direction);
-                    if (next.getType() != Material.RAIL) return;
+            var yaw = passenger.getLocation().getYaw();
+            var shape = getRailShape(direction, yaw);
 
-                    // ascending tracks can't be switched
-                    if (next.getBlockData().getAsString().contains(SHAPE_ASC)) return;
-
-                    var yaw = passenger.getLocation().getYaw();
-                    var shape = GetSwitchDirection(direction, yaw);
-
-                    var state = next.getState();
-                    var data = (Rail) state.getBlockData();
-                    data.setShape(shape);
-                    state.setBlockData(data);
-                    state.update();
-                }
-            }
+            setRailShape(next, shape);
         }
     }
 
-    private BlockFace GetMinecartDirection(Location speed)
+    private static boolean blockIsTwoWaySwitch(Block rails)
+    {
+        var bottom = rails.getRelative(BlockFace.DOWN);
+        return rails.getType() == Material.ACTIVATOR_RAIL && bottom.getType() == Material.IRON_BLOCK;
+    }
+
+    private static Location getVelocity(VehicleMoveEvent event)
+    {
+        return event.getTo().subtract(event.getFrom());
+    }
+
+    private static BlockFace getMinecartDirection(Location speed)
     {
         if /**/ (speed.getX() != 0) return speed.getX() > 0 ? BlockFace.EAST : BlockFace.WEST;
         else if (speed.getZ() != 0) return speed.getZ() > 0 ? BlockFace.SOUTH : BlockFace.NORTH;
@@ -63,7 +66,7 @@ public class MinecartListener implements Listener
         else return BlockFace.DOWN;
     }
 
-    private Rail.Shape GetSwitchDirection(BlockFace minecartDirection, float playerYaw)
+    private static Rail.Shape getRailShape(BlockFace minecartDirection, float playerYaw)
     {
         switch (minecartDirection)
         {
@@ -76,5 +79,14 @@ public class MinecartListener implements Listener
             default:
                 return Rail.Shape.NORTH_EAST;
         }
+    }
+
+    private static void setRailShape(Block rail, Rail.Shape shape)
+    {
+        var state = rail.getState();
+        var data = (Rail) state.getBlockData();
+        data.setShape(shape);
+        state.setBlockData(data);
+        state.update();
     }
 }
