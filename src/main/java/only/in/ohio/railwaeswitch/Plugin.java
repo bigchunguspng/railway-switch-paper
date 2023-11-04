95d0ed7e8ff47eeea72a0e5067071f6704b1a525
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-11-04 12:21:21 +0200

    #1

---
+++ src/main/java/only/in/ohio/railwaeswitch/Plugin.java
@@ -0,0 +1,19 @@
+package only.in.ohio.railwaeswitch;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        System.out.println("Using RAIL WAE SWITCH!!!!! [WHOLESOME 100 EPIC 25 TRANSPORT 89]");
+    }
+
+    @Override
+    public void onDisable()
+    {
+        System.out.println("No more RAIL WAE SWITCH!!! *BOOM!*");
+    }
+}
