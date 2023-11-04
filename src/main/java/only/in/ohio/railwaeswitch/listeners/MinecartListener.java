da5b6d69af979a32967c69d663233795cf79e61a
bigchungus <70759405+bigchunguspng@users.noreply.github.com>
2023-11-04 18:47:50 +0200

    rails are switched (right turn only)

--- src/main/java/only/in/ohio/railwaeswitch/listeners/MinecartListener.java
+++ src/main/java/only/in/ohio/railwaeswitch/listeners/MinecartListener.java
@@ -1,8 +1,10 @@
 package only.in.ohio.railwaeswitch.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Rail;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,8 +12,7 @@ import org.bukkit.event.vehicle.VehicleMoveEvent;
 
 public class MinecartListener implements Listener
 {
-    final String SHAPE_EW = "shape=east_west";
-    //final String SHAPE_NS = "shape=north_south";
+    final String SHAPE_ASC = "shape=ascending";
 
     @EventHandler
     public void onVehicleMove(VehicleMoveEvent event)
@@ -29,19 +30,51 @@ public class MinecartListener implements Listener
 
                 if (rails.getType() == Material.ACTIVATOR_RAIL && floor.getType() == Material.IRON_BLOCK)
                 {
-                    var ew = rails.getBlockData().getAsString().contains(SHAPE_EW); // rails go along X axis
+                    var velocity = event.getTo().subtract(event.getFrom());
+                    var direction = GetMinecartDirection(velocity);
 
-                    Bukkit.getLogger().info(ew ? "--X--" : "--Z--");
+                    Bukkit.getLogger().info(velocity.toString());
+                    Bukkit.getLogger().info(direction.toString());
 
-                    // get cart movement direction: EWSN
+                    var next = rails.getRelative(direction);
+                    if (next.getType() != Material.RAIL) return;
 
-                    // get rail block after switch block (return if it ain't no rail)
+                    // ascending tracks can't be switched
+                    if (next.getBlockData().getAsString().contains(SHAPE_ASC)) return;
 
-                    // get passenger POV
+                    var yaw = passenger.getLocation().getYaw();
+                    var shape = GetSwitchDirection(direction, yaw);
 
-                    // switch rails
+                    var state = next.getState();
+                    var data = (Rail) state.getBlockData();
+                    data.setShape(shape);
+                    state.setBlockData(data);
+                    state.update();
                 }
             }
+        }
+    }
+
+    private BlockFace GetMinecartDirection(Location speed)
+    {
+        if /**/ (speed.getX() != 0) return speed.getX() > 0 ? BlockFace.EAST : BlockFace.WEST;
+        else if (speed.getZ() != 0) return speed.getZ() > 0 ? BlockFace.SOUTH : BlockFace.NORTH;
+
+        else return BlockFace.DOWN;
+    }
+
+    private Rail.Shape GetSwitchDirection(BlockFace minecartDirection, float playerYaw)
+    {
+        switch (minecartDirection)
+        {
+            case NORTH:
+                return Rail.Shape.SOUTH_EAST;
+            case EAST:
+                return Rail.Shape.SOUTH_WEST;
+            case SOUTH:
+                return Rail.Shape.NORTH_WEST;
+            default:
+                return Rail.Shape.NORTH_EAST;
         }
     }
 }
